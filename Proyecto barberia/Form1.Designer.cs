e9d400fc1e29e268b7dd2e7633edc0f01d4abeff
galactic10 <gustavorios1425@gmail.com>
2026-02-24 12:54:12 -0700

    Agregue dos forms

--- Proyecto barberia/Form1.Designer.cs
+++ Proyecto barberia/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Proyecto_barberia
+﻿using System.Windows.Forms;
+
+namespace Proyecto_barberia
 {
     partial class Form1
     {
@@ -28,10 +30,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
